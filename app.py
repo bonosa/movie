ec1a9c2f2bdd36204d482a0112727fb6db32fa89
bonosa <53297623+bonosa@users.noreply.github.com>
2023-05-19 20:32:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,52 @@
 import streamlit as st
 import pandas as pd
 from sentence_transformers import SentenceTransformer
-from annoy import AnnoyIndex
-url='https://storage.googleapis.com/movves123/movies.csv'
+import numpy as np
+import scipy.spatial
 
+# Load the BERT model. Various models trained on Natural Language Inference (NLI) https://www.sbert.net/docs/pretrained_models.html#nli-models
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 @st.cache(allow_output_mutation=True)
-def load_data(url):
-    df = pd.read_csv(url)
+def load_data():
+    df = pd.read_csv('https://storage.googleapis.com/movves123/movies.csv')
     return df
 
-df = load_data(url)
+df = load_data()
 
+# We get all unique sentences
+sentences = df['title'].unique()
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    return model
+# Compute embedding for both lists
+embeddings = model.encode(sentences, convert_to_tensor=True)
 
-@st.cache(allow_output_mutation=True)
-def create_index(df, model):
-    embeddings = model.encode(df['genres'].tolist())
-    f = len(embeddings[0])
-    t = AnnoyIndex(f, 'angular')
-    for i, v in enumerate(embeddings):
-        t.add_item(i, v)
-    t.build(10)
-    return t
+# Compute cosine-similarities for each sentence with each other sentence
+cosine_scores = scipy.spatial.distance.cdist(embeddings, embeddings, "cosine")
 
-def get_recommendations(query, df, model, index):
-    query_embedding = model.encode([query])[0]
-    indices = index.get_nns_by_vector(query_embedding, 6)
-    recommendations = df['title'].iloc[indices]
-    return recommendations
+# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+top_k = min(5, len(sentences))
 
-st.title('Movie Recommendation App')
+st.title("Movie Recommender")
+user_input = st.text_input("Enter a genre", "Action")
 
-uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
-if uploaded_file is not None:
-    df = load_data(uploaded_file)
-    model = load_model()
-    index = create_index(df, model)
+if st.button('Submit'):
+    # Get the query embeddings
+    queries = [user_input]
+    query_embeddings = model.encode(queries, convert_to_tensor=True)
+    
+    # For each query, we compute the cosine-similarity scores with every sentence in the corpus
+    for query, query_embedding in zip(queries, query_embeddings):
+        distances = scipy.spatial.distance.cdist([query_embedding], embeddings, "cosine")[0]
 
-    st.write('Example genres: Action, Comedy, Romance')
-    query = st.text_input('Enter a genre:')
-    if st.button('Submit'):
-        if query:
-            recommendations = get_recommendations(query, df, model, index)
-            st.write(recommendations)
+        results = zip(range(len(distances)), distances)
+        results = sorted(results, key=lambda x: x[1])
+
+        st.write("\n\n======================\n\n")
+        st.write("Query:", query)
+        st.write("\nTop 5 most similar sentences in corpus:")
+
+        for idx, distance in results[0:top_k]:
+            st.write(sentences[idx].strip(), "(Score: %.4f)" % (1-distance))
+
+        st.write("\n\n======================\n\n")
+
